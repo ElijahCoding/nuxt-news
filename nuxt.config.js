fca9efefee07340ebbe28c66cff8f190fc8f2b05
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-21 15:41:06 +0300

    building login

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,6 +61,11 @@ export default {
       "/register/": {
           target: "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD_qByK8iAxoqOQG97QhY-e0bML66DwFtg",
           pathRewrite: { "^/register/": "" }
+      },
+
+      "/login/": {
+          target: "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyD_qByK8iAxoqOQG97QhY-e0bML66DwFtg",
+          pathRewrite: { "^/login/": "" }
       }
   },
 
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,7 +3,80 @@
 </template>
 
 <script>
+    import { validationMixin } from 'vuelidate';
+    import { required, email, minLength, maxLength } from 'vuelidate/lib/validators';
+
     export default {
-        middleware: 'auth'
+        middleware: 'auth',
+        mixins: [validationMixin],
+
+        data () {
+            return {
+                form: {
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+
+        validations: {
+            form: {
+                email: {
+                    required,
+                    email
+                },
+                password: {
+                    required,
+                    minLength: minLength(6),
+                    maxLength: maxLength(20)
+                }
+            }
+        },
+
+        computed: {
+            loading () {
+                return this.$store.getters.loading;
+            },
+
+            isAuthenticated () {
+                return this.$store.getters.isAuthenticated;
+            }
+        },
+
+        watch: {
+            isAuthenticated (value) {
+                if (value) {
+                    setTimeout(() => this.$router.push('/'), 2000);
+                }
+            }
+        },
+
+        methods: {
+            validateForm () {
+                this.$v.$touch();
+
+                if (!this.$v.$invalid) {
+                    this.registerUser();
+                }
+            },
+
+            async registerUser () {
+                await this.$store.dispatch('authenticateUser', {
+                    email: this.form.email,
+                    password: this.form.password,
+                    returnSecureToken: true
+                });
+            },
+
+            getValidationClass (fieldName) {
+                const field = this.$v.form[fieldName];
+
+                if (field) {
+                    return {
+                        "md-invalid": field.$invalid && field.$dirty
+                    }
+                }
+            }
+        }
     }
 </script>
